8e7eda74e9678ed0c6fefcef02b6a08a35c5d48a
fgotelip <felipegotdelga@gmail.com>
2025-02-27 18:53:15 -0300

    t2

--- Code/exemplo.py
+++ Code/exemplo.py
@@ -6,12 +6,12 @@ from Deflexao import Deflexao
 retangulos=[Retangulo(0,100,0,100)]
 buracos=[]
 
-'''carregamentos1 = [ Carregamento(0,5,150,1,150),Carregamento(5,5,-100,3),Carregamento(5,9,110,2,0,4)]
+carregamentos1 = [ Carregamento(0,5,150,1,150),Carregamento(5,5,-100,3),Carregamento(5,9,110,2,0,4)]
 apoios1 = [Apoio(0,3),0]
 
 deflexao1 = Deflexao(retangulos,buracos,carregamentos1,apoios1)
 deflexao1.plot_deflexao()
-'''
+
 
 '''carregamentos2 = [ Carregamento(0,3,-15,3,0,3),Carregamento(3,6,30,1,60)]
 apoios2 = [Apoio(0,2),Apoio(6,1)]
@@ -26,6 +26,6 @@ deflexao3 = Deflexao(retangulos,buracos,carregamentos3,apoios3)
 deflexao3.plot_deflexao()'''
 
 
-deflexao4 = Deflexao()
+'''deflexao4 = Deflexao()
 deflexao4.set_figura()
-deflexao4.plot_deflexao()
+deflexao4.plot_deflexao()'''
